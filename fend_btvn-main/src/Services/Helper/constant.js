dfd1e0719c3c49dba0a7cf28418aa86881349d7b
Hưng Nguyễn <hung131119782002@gmail.com>
2023-11-13 11:21:29 +0700

    add

---
+++ fend_btvn-main/src/Services/Helper/constant.js
@@ -0,0 +1,20 @@
+import {API_URL} from '@env';
+export const REST_API_URL = 'https://0f2e-2001-ee0-45d2-4b30-e40d-1204-78eb-c26b.ngrok-free.app/it4788';
+//export const REST_API_URL = API_URL;
+const INTERNET_CONNECTION_FAILED = 'Bạn đang offline';
+const INTERNET_CONNECTION_SUCCESS = 'Bạn đang online';
+const BLUE_COLOR = "#216fdb";
+const LIKE_BLUE_COLOR = "#30a4f0";
+const GRAY_COLOR_BACKGROUND = "#dbdbdd"
+const GRAY_TEXT_COLOR = "#8c8d90"
+
+export const COMMON_MESSAGE = {
+    INTERNET_CONNECTION_FAILED,
+    INTERNET_CONNECTION_SUCCESS
+}
+export const COMMON_COLOR = {
+    BLUE_COLOR,
+    LIKE_BLUE_COLOR,
+    GRAY_COLOR_BACKGROUND,
+    GRAY_TEXT_COLOR
+}
